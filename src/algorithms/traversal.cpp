ef003ee42e22110bfabd80b7ba512c23cbc50442
Mohamedazizjridi <153727546+Mohamedazizjridi@users.noreply.github.com>
2025-11-10 12:42:10 +0100

    Update traversal.cpp

--- src/algorithms/traversal.cpp
+++ src/algorithms/traversal.cpp
@@ -1,18 +1,17 @@
 #include "algorithms/traversal.hpp"
-#include <queue>
-#include <unordered_set>
-#include <stack>
+#include <bits/stdc++.h>
+using namesapce std;
 
 namespace graphviso {
 namespace algorithms {
 
 void BFS::traverse(const Graph& graph, int startNodeId, VisitCallback callback) {
-    std::unordered_set<int> visited;
-    std::queue<std::shared_ptr<Node>> queue;
+    unordered_set<int> visited;
+    queue<shared_ptr<Node>> queue;
     
     auto nodes = graph.getNodes();
-    auto start = std::find_if(nodes.begin(), nodes.end(),
-        [startNodeId](const std::shared_ptr<Node>& node) {
+    auto start = find_if(nodes.begin(), nodes.end(),
+        [startNodeId](const shared_ptr<Node>& node) {
             return node->getId() == startNodeId;
         });
         
@@ -26,10 +25,7 @@ void BFS::traverse(const Graph& graph, int startNodeId, VisitCallback callback) 
     while (!queue.empty()) {
         auto current = queue.front();
         queue.pop();
-        
         callback(*current);
-        
-        // Find all neighbors
         for (const auto& edge : graph.getEdges()) {
             if (edge->getSource()->getId() == current->getId()) {
                 auto neighbor = edge->getTarget();
@@ -50,11 +46,11 @@ void BFS::traverse(const Graph& graph, int startNodeId, VisitCallback callback) 
 }
 
 void DFS::traverse(const Graph& graph, int startNodeId, VisitCallback callback) {
-    std::unordered_set<int> visited;
-    std::stack<std::shared_ptr<Node>> stack;
+    unordered_set<int> visited;
+    stack<std::shared_ptr<Node>> stack;
     
     auto nodes = graph.getNodes();
-    auto start = std::find_if(nodes.begin(), nodes.end(),
+    auto start = find_if(nodes.begin(), nodes.end(),
         [startNodeId](const std::shared_ptr<Node>& node) {
             return node->getId() == startNodeId;
         });
@@ -68,16 +64,12 @@ void DFS::traverse(const Graph& graph, int startNodeId, VisitCallback callback) 
     while (!stack.empty()) {
         auto current = stack.top();
         stack.pop();
-        
         if (visited.find(current->getId()) != visited.end()) {
             continue;
         }
-        
         visited.insert(current->getId());
         callback(*current);
-        
-        // Find all neighbors (push in reverse order to maintain expected traversal order)
-        std::vector<std::shared_ptr<Node>> neighbors;
+        vector<shared_ptr<Node>> neighbors;
         for (const auto& edge : graph.getEdges()) {
             if (edge->getSource()->getId() == current->getId()) {
                 auto neighbor = edge->getTarget();
